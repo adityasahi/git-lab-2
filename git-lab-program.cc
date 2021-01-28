84e6821df76473163b84a691acd38c993855c8d7
Aditya Sahi <as301518@ohio.edu>
2021-01-27 19:57:00 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *      File: git-lab-program.cc
+ *      Author: Aditya Sahi
+ *      Date: 01/27/2021
+ *      Description: This program is for git lab and it is lab 2
+ */
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+} // main
